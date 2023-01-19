8917fa6d2a3e601827c911f58dacc89f115351ec
So Hyun (Esther) Choi <estherchoi.sh@gmail.com>
2023-01-19 02:22:41 +0000

    Implement diagonal test

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,37 +1,119 @@
-const chai = require('chai');
+const chai = require("chai");
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require("../wordsearch.js");
 
-describe("#wordSearch()", function() {
-  it("should return false if the word is not present", function() {
-    const result = wordSearch([
-      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
-      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
-      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
-      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
-      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
-      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
-      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+const template = [
+  ["A", "W", "C", "F", "Q", "U", "A", "L"],
+  ["S", "E", "I", "N", "F", "E", "L", "D"],
+  ["Y", "F", "C", "F", "Q", "U", "A", "L"],
+  ["H", "M", "J", "T", "E", "V", "R", "G"],
+  ["W", "H", "C", "S", "Y", "E", "R", "L"],
+  ["B", "F", "R", "E", "N", "E", "Y", "B"],
+  ["U", "B", "T", "W", "A", "P", "A", "I"],
+  ["O", "D", "C", "A", "K", "U", "A", "S"],
+  ["E", "Z", "K", "F", "Q", "U", "A", "L"],
+];
+
+describe("#wordSearch()", function () {
+  it("should return false if the word is not present (horizontal)", function () {
+    const result = wordSearch(template, "FRANK");
 
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
-    const result = wordSearch([
-      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
-      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
-      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
-      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
-      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
-      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
-      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+  it("should return true if the word is present (horizontal)", function () {
+    const result = wordSearch(template, "SEINFELD");
+
+    assert.isTrue(result);
+  });
+
+  it("word is not case sensitive", function () {
+    const result = wordSearch(template, "Seinfeld");
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if the word is not present (vertical)", function () {
+    const result = wordSearch(template, "GEORGE");
+
+    assert.isFalse(result);
+  });
+
+  it("should return true if the word is present (vertical)", function () {
+    const result = wordSearch(template, "LARRY");
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if the word is not present (horizonal - backwards)", function () {
+    const result = wordSearch(template, "ESTHER");
+
+    assert.isFalse(result);
+  });
+
+  it("should return true if the word is present (horizontal - backward)", function () {
+    const result = wordSearch(template, "YENER");
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if the word is not present (vertical - backwards)", function () {
+    const result = wordSearch(template, "CODY");
+
+    assert.isFalse(result);
+  });
+
+  it("should return true if the word is present (vertical - backward)", function () {
+    const result = wordSearch(template, "WEST");
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if the word is not present (diagonal down)", function () {
+    const result = wordSearch(template, "BOBBY");
+
+    assert.isFalse(result);
+  });
+
+  it("should return true if the word is present (diagonal down)", function () {
+    const result = wordSearch(template, "WIFE");
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if the word is not present (diagonal up)", function () {
+    const result = wordSearch(template, "BILLY");
+
+    assert.isFalse(result);
+  });
+
+  it("should return true if the word is present (diagonal up)", function () {
+    const result = wordSearch(template, "SEUL");
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if the word is not present (diagonal down - backwards)", function () {
+    const result = wordSearch(template, "BOBBY");
+
+    assert.isFalse(result);
+  });
+
+  it("should return true if the word is present (diagonal down - backward)", function () {
+    const result = wordSearch(template, "YEEF");
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if the word is not present (diagonal up - backwards)", function () {
+    const result = wordSearch(template, "BILLY");
+
+    assert.isFalse(result);
+  });
+
+  it("should return true if the word is present (diagonal up - backward)", function () {
+    const result = wordSearch(template, "AVYET");
 
     assert.isTrue(result);
   });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,100 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const transpose = (matrix) => {
+  let resultArray = [];
 
-module.exports = wordSearch
+  for (let i = 0; i < matrix[0].length; i++) {
+    resultArray.push([]);
+  }
+
+  for (const col in matrix) {
+    for (const row in matrix[col]) {
+      resultArray[row][col] = matrix[col][row];
+    }
+  }
+
+  return resultArray;
+};
+
+const lookForWord = (array, word) => {
+  for (const l of array) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
+
+const makeDiagonalDownArray = (letters, array) => {
+  const word = [];
+  const helper = (firstIndex, secondIndex) => {
+    if (firstIndex === letters.length || secondIndex === letters[0].length) {
+      return;
+    }
+
+    word.push(letters[firstIndex][secondIndex]);
+    helper(firstIndex + 1, secondIndex + 1);
+  };
+
+  for (let i = 0; i < letters.length; i++) {
+    helper(i, 0);
+    array.push(word.join(""));
+    word.splice(0);
+  }
+
+  for (let j = 1; j < letters[0].length; j++) {
+    helper(0, j);
+    array.push(word.join(""));
+    word.splice(0);
+  }
+};
+
+const makeDiagonalUpArray = (letters, array) => {
+  const word = [];
+  const helper = (firstIndex, secondIndex) => {
+    if (firstIndex < 0 || secondIndex === letters[0].length) {
+      return;
+    }
+
+    word.push(letters[firstIndex][secondIndex]);
+    helper(firstIndex - 1, secondIndex + 1);
+  };
+
+  for (let i = letters.length - 1; i >= 0; i--) {
+    helper(i, 0);
+    array.push(word.join(""));
+    word.splice(0);
+  }
+
+  for (let j = 1; j < letters[0].length; j++) {
+    helper(8, j);
+    array.push(word.join(""));
+    word.splice(0);
+  }
+};
+
+const wordSearch = (letters, word) => {
+  const wordUpper = word.toUpperCase();
+  const wordUpperReverse = wordUpper.split("").reverse().join("");
+
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  const transposedArray = transpose(letters);
+  const verticalJoin = transposedArray.map((ls) => ls.join(""));
+
+  let diagonalDownJoin = [];
+  makeDiagonalDownArray(letters, diagonalDownJoin);
+  let diagonalUpJoin = [];
+  makeDiagonalUpArray(letters, diagonalUpJoin);
+
+  if (
+    lookForWord(horizontalJoin, wordUpper) ||
+    lookForWord(verticalJoin, wordUpper) ||
+    lookForWord(horizontalJoin, wordUpperReverse) ||
+    lookForWord(verticalJoin, wordUpperReverse) ||
+    lookForWord(diagonalUpJoin, wordUpper) ||
+    lookForWord(diagonalDownJoin, wordUpper) ||
+    lookForWord(diagonalUpJoin, wordUpperReverse) ||
+    lookForWord(diagonalDownJoin, wordUpperReverse)
+  ) {
+    return true;
+  }
+  return false;
+};
+
+module.exports = wordSearch;
